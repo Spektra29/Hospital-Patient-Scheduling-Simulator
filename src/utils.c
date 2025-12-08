6ff6e73132c9bde44a54cb0529f659a622e40baa
Victor Liu <vliu9313@gmail.com>
2025-12-07 19:59:55 -0500

    change utils.c to change output format

--- src/utils.c
+++ src/utils.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
 #include "../include/scheduler.h"
 
+// Simple swap helper
+void swap(Patient *a, Patient *b) {
+    Patient temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+// Sort patients by completion time (ascending)
+void sort_by_completion(Patient patients[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (patients[j].completion > patients[j + 1].completion) {
+                swap(&patients[j], &patients[j + 1]);
+            }
+        }
+    }
+}
+
 void print_results(Patient patients[], int n, const char *title) {
+    sort_by_completion(patients, n);  // Sort before printing
+
     printf("\n===== %s =====\n", title);
     printf("ID\tArrival\tBurst\tPriority\tWaiting\tTurnaround\n");
     for (int i = 0; i < n; i++) {
@@ -14,6 +34,7 @@ void print_results(Patient patients[], int n, const char *title) {
                patients[i].turnaround);
     }
 }
+
 
 void copy_patients(Patient dst[], Patient src[], int n) {
     for (int i = 0; i < n; i++) {
